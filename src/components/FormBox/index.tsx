841ec7f64453c9e71dd40d52afc3fa4bf02b971e
Lucas de Lima <lms.souza39@gmail.com.br>
2022-07-24 22:23:31 -0300

    :sparkles: adição do firestore, uso para adição de dados no projeto firesto :sparkles:

--- src/components/FormBox/index.tsx
+++ src/components/FormBox/index.tsx
@@ -1,20 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
+import firestore from '@react-native-firebase/firestore'
+import 'react-native-get-random-values';
+import { v4 as uuid } from 'uuid';
+import { Alert } from 'react-native';
 
-import { Container } from './styles';
 import { ButtonIcon } from '../ButtonIcon';
 import { Input } from '../Input';
 
+import { Container } from './styles';
+
 export function FormBox() {
+  const [description, setDescription] = useState('');
+  const [quantity, setQuantity] = useState(0);
+
+  async function handleSubmit() {
+    try {
+      const id = uuid();
+      const data = {
+        description,
+        quantity,
+        createdAt: new Date(),
+      }
+      await firestore().collection('products').doc(id).set(data);
+      setDescription('');
+      setQuantity(0);
+      Alert.alert('Sucesso ✅', 'Produto cadastrado com sucesso!');
+    } catch (error) {
+      console.log(error);
+      Alert.alert('Erro ❌ ', 'Erro ao cadastrar produto');
+    }
+  }
+
   return (
     <Container>
       <Input
         placeholder="Nome do produto"
         size="medium"
+        onChangeText={setDescription}
       />
 
       <Input
         placeholder="0"
         keyboardType="numeric"
+        onChangeText={value => setQuantity(Number(value))}
         size="small"
         style={{ marginHorizontal: 8 }}
       />
@@ -22,7 +50,7 @@ export function FormBox() {
       <ButtonIcon
         size='large'
         icon="add-shopping-cart"
-        onPress={() => { }}
+        onPress={handleSubmit}
       />
     </Container>
   );
